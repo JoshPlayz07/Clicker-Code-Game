26a49adb15cc4c9256d7a39159bca7adf049bbf6
JoshPlayz07 <94185839+JoshPlayz07@users.noreply.github.com>
2021-11-25 21:44:52 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,16 +20,13 @@ input.onPinPressed(TouchPin.P0, function () {
             . # . # .
             # . . . #
             `)
+        basic.pause(1000)
         basic.clearScreen()
     }
 })
 // Add Points Per Click
 input.onButtonPressed(Button.A, function () {
     game.addScore(points_per_click)
-})
-// View Score
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(game.score())
 })
 // Level Up
 input.onPinPressed(TouchPin.P2, function () {
@@ -52,19 +49,42 @@ input.onPinPressed(TouchPin.P2, function () {
         basic.showString("Points")
     }
 })
-// Try Put The second Upgrade here
-input.onPinPressed(TouchPin.P1, function() {
-    if (game.score() >= upgrade_2_cost) { 
+// View Score
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(game.score())
+})
+// Upgrade 2
+input.onPinPressed(TouchPin.P1, function () {
+    if (game.score() >= upgrade_2_cost) {
         basic.showLeds(`
-        . . . . .
-        . . . . #
-        . . . # .
-        # . # . .
-        . # . . .
-        `)
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
         game.addScore(upgrade_2_remove_cost)
+        times_2nd_upgrade_used += 1
+        points_per_second = 0 + times_2nd_upgrade_used
+        upgrade_1_cost = upgrade_1_cost + 200
+        upgrade_1_remove_cost = upgrade_1_remove_cost - 200
+        basic.forever(function () {
+            basic.pause(1000)
+            game.addScore(points_per_second)
+        })
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
     }
 })
+let times_2nd_upgrade_used = 0
 let level = 0
 let points_per_click = 0
 let upgrade_1_remove_cost = 0
@@ -72,6 +92,8 @@ let upgrade_1_cost = 0
 let level_cost = 0
 let upgrade_2_cost = 0
 let upgrade_2_remove_cost = 0
+let points_per_second2 = 0
+let points_per_second = 0
 upgrade_2_remove_cost = -200
 upgrade_2_cost = 200
 level_cost = 1000
